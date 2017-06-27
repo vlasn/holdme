7c752540c1779134421ce3b22b179642b78fa24c
Veljo Lasn <vlasn@tlu.ee>
2017-06-27 15:29:19 +0300

    Improvevmenti

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-let express = require("express")
-    axios = require('axios'),
-    express = require('express'),
-    API_TOKEN = process.env.API_TOKEN,
-    TICKET_FIELD_HASH = process.env.TICKET_FIELD_HASH,
-    SLACK_FIELD_HASH = process.env.SLACK_FIELD_HASH,
-    WEBHOOK_URL = process.env.WEBHOOK_URL,
+const express = require("express")
     PORT=process.env.PORT
     bodyParser = require('body-parser')
-
-
+    
 let app = express()
-
 app.use(bodyParser.json())
-
 
 const { 
     buildMessage, postToSlack, getSlackUsername ,ensureAt, greeting, filter 
-} = require("./sketch")
+} = require("./utilities")
 
 app.post('/hold', (req, res) => {
     getSlackUsername(req.body.current.person_id)
